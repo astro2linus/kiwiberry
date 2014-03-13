4ad115d34d7981839e9a165d1f03b3e670c8ac91
astro2linus <astro2linus@gmail.com>
2014-03-13 17:58:46 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kiwiberry::Application.config.secret_key_base = 'bcb1a2d38d52fb9f6e2255238320905283c4cd7457a813b0c45b647fb488c75ca393abd1861b6b149ca14824e89a28ff0308e661b8624428082119d9ea18d748'
